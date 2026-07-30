6fa418740fd0de5874a23d183540e13cba588d22
manana <manana@Mac-mini-de-administrator.local>
2026-07-30 12:50:01 +0200

    iconos y estilos

--- src/terminal/components/contact/ContactPanel.tsx
+++ src/terminal/components/contact/ContactPanel.tsx
@@ -1,17 +1,18 @@
 import { contact } from "../../data/contact";
 import './ContactPanel.css'
-import {SiGithub} from "react-icons/si";
+import {SiGithub, SiGmail} from "react-icons/si";
 import { FaLinkedin, FaLinkedinIn } from 'react-icons/fa';
 
 
 const ContactPanel = () => {
   return (
     <>
-      <a className="contact-links" href={`mailto:${contact.email}`} target="_blank">✉️ Enviame un correo</a>
+    <p style={{ color: "#febc2e" }}>CONTACTO:</p>
+      <a className="contact-links" href={`mailto:${contact.email}`} target="_blank"><SiGmail style={{ color: '#c72c0d', fontSize: '20px' }} target="_blank"/> Enviame un correo</a>
       <br /><br />
-      <a className="contact-links" href={contact.github}><SiGithub style={{ color: '#F0F6FC', fontSize: '20px' }} target="_blank"/>  Visita mi GitHub</a>
+      <a className="contact-links" href={contact.github} target="_blank"><SiGithub style={{ color: '#F0F6FC', fontSize: '22px' }}/>  Visita mi GitHub</a>
       <br /><br />
-      <a className="contact-links" href={contact.linkedin}><FaLinkedin style={{ color: '#F0F6FC', fontSize: '20px' }} target="_blank"/> Encuentrame en LinkedIn</a>
+      <a className="contact-links" href={contact.linkedin} target="_blank"><FaLinkedin style={{ color: '#0A66C2', fontSize: '22px' }}/> Encuentrame en LinkedIn</a>
       <br /><br />
       <a className="contact-links" href={contact.cv} target="_blank">📄 Descarga mi CV</a>
 
